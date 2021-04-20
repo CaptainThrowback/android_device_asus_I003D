734f8f2b89bd4aa37f9f998d0bf07296a0dc21b1
Captain Throwback <captainthrowback@hotmail.com>
2021-04-20 14:27:43 -0400

    I003D: update for 11 tree and twrp-specific vendor
    Change-Id: Id85ab96ebbe37f1120cb1d3e02747822db1ca0d4

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_I003D.mk
+    $(LOCAL_DIR)/twrp_I003D.mk
--- twrp_I003D.mk
+++ twrp_I003D.mk
@@ -14,11 +14,14 @@
 # limitations under the License.
 #
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Custom vendor used in build tree (automatically taken from prefix of this filename)
+CUSTOM_VENDOR := $(lastword $(subst /, ,$(firstword $(subst _, ,$(firstword $(MAKEFILE_LIST))))))
 
-# OEM Info
-BOARD_VENDOR := asus
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/$(CUSTOM_VENDOR)/config/common.mk)
+
+# OEM Info (automatically taken from device tree path)
+BOARD_VENDOR := $(or $(word 2,$(subst /, ,$(firstword $(MAKEFILE_LIST)))),$(value 2))
 
 # Identifier for common folder
 COMMON_SOC := sm8250
@@ -31,7 +34,7 @@ PRODUCT_RELEASE_NAME := I003D
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
-PRODUCT_NAME := omni_$(PRODUCT_DEVICE)
+PRODUCT_NAME := $(CUSTOM_VENDOR)_$(PRODUCT_DEVICE)
 PRODUCT_BRAND := $(BOARD_VENDOR)
 PRODUCT_MODEL := $(shell echo $(PRODUCT_BRAND) | tr  '[:lower:]' '[:upper:]')_$(PRODUCT_DEVICE)
 PRODUCT_MANUFACTURER := $(PRODUCT_BRAND)
